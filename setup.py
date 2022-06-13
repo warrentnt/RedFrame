a47b061d3e3faa9753cd72a2d27b53b91c05a13d
Warren Thompson <warrentnt@gmail.com>
2022-06-13 01:01:02 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RedFrame',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Free and open source',
+    author='Warren Thompson',
+    author_email='Warren.Thompson@trojans.dsu.edu',
+    description='RedFrame is intended to be a basic Red Teaming framing designed to allow the user to enumerate and sniff a target network'
+)
